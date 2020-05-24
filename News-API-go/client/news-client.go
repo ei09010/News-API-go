8a807e401a9940ef3f78c8d44583936c64d7f272
mario <mario24jsp@gmail.com>
2020-05-24 02:20:16 +0100

    WIP: creating model objects and enums

---
+++ News-API-go/client/news-client.go
@@ -0,0 +1,58 @@
+package client
+
+import (
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type Category int
+
+const Categories (
+	Business Category iota
+	Entertainment
+	Health
+	Science
+	Sports
+	Technology
+)
+
+type Client struct {
+	BaseURL   *url.URL
+	UserAgent string
+
+	httpClient *http.Client
+}
+
+type Source struct {
+	Id   string
+	Name string
+}
+
+type Article struct {
+	SourceName  Source
+	Author      string
+	Title       string
+	Description string
+	Url         string
+	UrlToImage  string
+	PublishedAt Time
+}
+
+type ArticlesResult struct {
+
+	// buch os status
+	// bunch of error
+	TotalResults int
+	Articles     []Article
+}
+
+type TopHeadlinesRequest struct{
+	Keyword string
+	Sources []string
+
+}
+
+func (c *Client) GetTopHeadlines() (*ArticlesResult, error) {
+
+}
