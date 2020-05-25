ac74ab3c6cf25f5f9a8f4d1a58a71cae8ac8b162
mario <mario24jsp@gmail.com>
2020-05-25 23:54:07 +0100

    tweaked query params and still validating response objects

--- News-API-go/client/news-client.go
+++ News-API-go/client/news-client.go
@@ -5,7 +5,41 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 )
+
+type Source struct {
+	Id   string
+	Name string
+}
+
+type Article struct {
+	SourceName  Source
+	Author      string
+	Title       string
+	Description string
+	Url         string
+	UrlToImage  string
+	PublishedAt time.Time
+}
+
+type ArticlesResult struct {
+
+	// buch os status
+	// bunch of error
+	TotalResults int
+	Articles     []Article
+}
+
+type TopHeadlinesRequest struct {
+	Keyword  string
+	Sources  []string
+	Category Category
+	Language Language
+	Country  Country
+	Page     int
+	PageSize int
+}
 
 type Client struct {
 	BaseURL   *url.URL
@@ -14,23 +48,30 @@ type Client struct {
 	httpClient *http.Client
 }
 
-func (c *Client) GetTopHeadlines(req TopHeadlinesRequest) (ArticlesResult, error) {
+func (c *Client) GetTopHeadlines(req TopHeadlinesRequest) (*ArticlesResult, error) {
 
 	rel := &url.URL{Path: "/topheadlines"}
 
 	queryParams := []string{}
 
-	if req.KeyWord != "" {
-		append(queryParams, "q="+req.KeyWord)
+	if req.Keyword != "" {
+		queryParams = append(queryParams, "q="+req.Keyword)
 	}
 
 	if len(req.Sources) > 0 {
-		append(queryParams, "sources="+strings.Join(req.Sources[:], ","))
+		queryParams = append(queryParams, "sources="+strings.Join(req.Sources[:], ","))
 	}
+
+	// to add further query parameters
+
+	queryString := strings.Join(queryParams[:], "&")
+
+	rel.Path += queryString
 
 	u := c.BaseURL.ResolveReference(rel)
 
 	customReq, err := http.NewRequest("GET", u.String(), nil)
+
 	if err != nil {
 		return nil, err
 	}
@@ -38,12 +79,16 @@ func (c *Client) GetTopHeadlines(req TopHeadlinesRequest) (ArticlesResult, error
 	customReq.Header.Set("User-Agent", c.UserAgent)
 
 	resp, err := c.httpClient.Do(customReq)
+
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
-	var users []User
-	err = json.NewDecoder(resp.Body).Decode(&users)
-	return users, err
+
+	var articleResults ArticlesResult
+
+	err = json.NewDecoder(resp.Body).Decode(&articleResults)
+
+	return &articleResults, err
 
 }
