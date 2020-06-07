50020765ad65e2b53bbd0c388b997fac90e9779a
Jorge Pereira <jorge.pereira@farfetch.com>
2020-06-07 01:04:45 +0100

    first unit test in progress

---
+++ client/news-client_test.go
@@ -0,0 +1,17 @@
+package client
+
+import "testing"
+
+func TestGetTopHeadlines_StandardRequest_ReturnsSuccessResponse(t *testing.T) {
+
+	//Arrange
+	// check with postman what expected json should be returned
+	// call to endpoint has to be mocked, check which options are available in Golang
+
+	// Act
+	// build object with the request for execution
+
+	// Assert
+	// Assert result
+
+}
